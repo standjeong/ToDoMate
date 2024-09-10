be120589ab7db1a73294f1beec806c95294617ce
standjeong <andame9999@gmail.com>
2024-09-10 22:42:41 +0900

    feat: Display to-do list

--- src/components/today/TodayPlanner.jsx
+++ src/components/today/TodayPlanner.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import AddTodo from './AddTodo';
 import Header from './Header';
 import TodoList from './TodoList';
 
 export default function TodayPlanner() {
+  const [todos, setTodos] = useState(initialVal);
+
   return (
     <section>
       <Header />
-      <TodoList />
+      <TodoList todos={todos} />
       <AddTodo />
     </section>
   );
 }
+
+const initialVal = [
+  { id: 1, text: '프로젝트끝내기', completed: false },
+  { id: 2, text: '포폴작업', completed: false },
+  { id: 3, text: '청소하기', completed: false },
+];
--- src/components/today/TodoList.jsx
+++ src/components/today/TodoList.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Todo from './Todo';
 
-export default function TodoList() {
-  return <div>투두목록</div>;
+export default function TodoList({ todos }) {
+  return (
+    <ul>
+      {todos && todos.map((todo, index) => <Todo key={index} todo={todo} />)}
+    </ul>
+  );
 }
